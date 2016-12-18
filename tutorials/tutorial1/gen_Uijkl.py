188d683cc11e214c56c5256263f2b3a240dca97a
DOMINIQUE GEFFROY <geffroy.dominique@free.fr>
2016-12-18 19:44:48 +0100

    fectch upstream

--- tutorials/tutorial1/gen_Uijkl.py
+++ tutorials/tutorial1/gen_Uijkl.py
@@ -4,6 +4,7 @@ import sys
 up = 0
 down = 1
 
+
 def complex_to_str(z):
     return "(%e,%e)"%(z.real,z.imag)
 
@@ -20,10 +21,12 @@ def generate_U_tensor_SK(n_orb, U, JH):
                     if iorb1==iorb2 and iorb2==iorb3 and iorb3==iorb4:
                         coeff = U
                     elif iorb1==iorb4 and iorb2==iorb3 and iorb1!=iorb2:
+                        # this is U', i.e. dd interaction between different orbs
                         coeff = U-2*JH
                     elif iorb1==iorb3 and iorb2==iorb4 and iorb1!=iorb2:
                         coeff = JH
                     elif iorb1==iorb2 and iorb3==iorb4 and iorb1!=iorb3:
+                        print iorb1, iorb2, iorb3, iorb4
                         coeff = JH
 
                     for isp in xrange(2):
@@ -31,16 +34,65 @@ def generate_U_tensor_SK(n_orb, U, JH):
                             U_tensor[iorb1,isp,    iorb2,isp2,    iorb3,isp2,  iorb4,isp] += coeff
                             if coeff != 0.0:
                                 num_elem += 1
-
+    print U_tensor[0, 0, 0, 1, 1, 0, 1, 1]
     return U_tensor, num_elem
 
-n_site = 3
-Uval = 1.0
-Jval = 0.25*Uval
+def generate_U_tensor_kunes(n_orb, U, JH, Jprime):
+    U_tensor = np.zeros((n_orb,2,n_orb,2,n_orb,2,n_orb,2),dtype=complex)
 
-V_mat = np.identity(2*n_site,dtype=complex)
+    num_elem = 0
+    for iorb1 in xrange(n_orb):
+        for iorb2 in xrange(n_orb):
+            for iorb3 in xrange(n_orb):
+                for iorb4 in xrange(n_orb):
+                    coeff = 0.0
+                    if iorb1==iorb2 and iorb2==iorb3 and iorb3==iorb4:
+                        coeff = U / 2
+                        for isp in xrange(2):
+                            isp2 = 1 - isp
+                            U_tensor[iorb1, isp,    iorb2, isp2,
+                                     iorb3, isp2,  iorb4, isp] = coeff
+                            if np.abs(coeff)>0:
+                                num_elem += 1
+                    elif iorb1==iorb4 and iorb2==iorb3 and iorb1!=iorb2:
+                        # this is U', i.e. dd interaction between different orbs
+                        coeff = (U - 2 * JH) / 2.0  # divide by 2 for ab neq ba
+                        for isp in xrange(2):
+                            for isp2 in xrange(2):
+                                U_tensor[iorb1, isp,    iorb2, isp2,
+                                        iorb3, isp2,  iorb4, isp] = coeff
+                                if np.abs(coeff)>0:
+                                    num_elem += 1                                    
+                        for isp in xrange(2):
+                            U_tensor[iorb1, isp,    iorb2, isp,
+                                        iorb3, isp,  iorb4, isp] -= JH / 2.0
+                    elif iorb1==iorb3 and iorb2==iorb4 and iorb1!=iorb2:
+                        coeff = JH / 2.0  # acount for exchange b <-> a
+                        for isp in xrange(2):
+                            isp2 = 1 - isp
+                            U_tensor[iorb1, isp, iorb2, isp2,
+                                     iorb3, isp2,  iorb4, isp] = coeff
+                            if np.abs(coeff)>0:
+                                num_elem += 1
+                    elif iorb1==iorb2 and iorb3==iorb4 and iorb1!=iorb3:
+                        coeff = Jprime / 2.0
+                        for isp in xrange(2):
+                            isp2 = 1 - isp
+                            U_tensor[iorb1, isp, iorb2, isp2,
+                                     iorb3, isp2,  iorb4, isp] = coeff
+                            if np.abs(coeff)>0:
+                                num_elem += 1
+    return U_tensor, num_elem
 
-U_tensor, num_elem = generate_U_tensor_SK(n_site, Uval, Jval)
+n_site = 2
+Uval = 2.0
+Jval = 0.25 * Uval
+#Jprime = 0.002
+Jprime = 0.00
+
+V_mat = np.identity(2 * n_site, dtype=complex)
+
+U_tensor, num_elem = generate_U_tensor_kunes(n_site, Uval, Jval, Jprime)
 
 f = open("Uijkl.txt", "w")
 print >>f, num_elem
--- tutorials/tutorial1/gen_hopping.py
+++ tutorials/tutorial1/gen_hopping.py
@@ -4,11 +4,14 @@ from scipy.integrate import simps
 from math import pi
 
 #Lambda = 1.0
-Lambda = 1.0
-nf=6
+# convention: a_up, a_down, b_up, b_down
+Lambda = 0.0
+nf=4
 norb=nf/2
-Uval = 1.0
+Uval = 8.0
 Jval = 0.25*Uval
+shifts = np.array([0.0, 0.0, -3.4, -3.4], dtype=complex)
+
 
 Himp = -0.5*Lambda*np.array( [
         [ 0,  0, -1J,  0,  0,  1],
@@ -18,9 +21,14 @@ Himp = -0.5*Lambda*np.array( [
         [ 0, -1,  0,  1J,  0,  0],
         [ 1,  0, 1J,   0,  0,  0]], dtype=complex)
 
-mu = 2.5*Uval-5*Jval
+# from b60/enforce_real
+mu = 1.3870789578665168
+
+#2.5*Uval-5*Jval
+
 for flavor in xrange(nf):
     Himp[flavor,flavor] -= mu
+    Himp[flavor,flavor] += shifts[flavor]
 
 f = open('hopping.txt','w')
 for iorb in xrange(nf):
