e6c664da0640707e5160d5ba461fd3968150d402
DOMINIQUE GEFFROY <geffroy.dominique@free.fr>
2019-08-06 16:03:50 +0200

    update gen_U scripts

--- tutorials/tuto_gfy/gen_SK.py
+++ tutorials/tuto_gfy/gen_SK.py
@@ -3,7 +3,7 @@ import sys
 
 up = 0
 down = 1
-
+eps = 0.00001
 
 def complex_to_str(z):
     return "(%e,%e)"%(z.real,z.imag)
@@ -13,10 +13,10 @@ def generate_U_tensor_SK(n_orb, U, JH):
     U_tensor = np.zeros((n_orb,2,n_orb,2,n_orb,2,n_orb,2),dtype=complex)
 
     num_elem = 0
-    for iorb1 in xrange(n_orb):
-        for iorb2 in xrange(n_orb):
-            for iorb3 in xrange(n_orb):
-                for iorb4 in xrange(n_orb):
+    for iorb1 in np.arange(n_orb):
+        for iorb2 in np.arange(n_orb):
+            for iorb3 in np.arange(n_orb):
+                for iorb4 in np.arange(n_orb):
                     coeff = 0.0
                     if iorb1==iorb2 and iorb2==iorb3 and iorb3==iorb4:
                         coeff = U
@@ -26,29 +26,28 @@ def generate_U_tensor_SK(n_orb, U, JH):
                     elif iorb1==iorb3 and iorb2==iorb4 and iorb1!=iorb2:
                         coeff = JH
                     elif iorb1==iorb2 and iorb3==iorb4 and iorb1!=iorb3:
-                        print iorb1, iorb2, iorb3, iorb4
+                        print (iorb1, iorb2, iorb3, iorb4)
                         coeff = JH
 
-                    for isp in xrange(2):
-                        for isp2 in xrange(2):
+                    for isp in np.arange(2):
+                        for isp2 in np.arange(2):
                             U_tensor[iorb1,isp,    iorb2,isp2,    iorb3,isp2,  iorb4,isp] += coeff
                             if coeff != 0.0:
                                 num_elem += 1
-    print U_tensor[0, 0, 0, 1, 1, 0, 1, 1]
     return U_tensor, num_elem
 
 def generate_U_tensor_kunes(n_orb, U, JH, Jprime):
     U_tensor = np.zeros((n_orb,2,n_orb,2,n_orb,2,n_orb,2),dtype=complex)
 
     num_elem = 0
-    for iorb1 in xrange(n_orb):
-        for iorb2 in xrange(n_orb):
-            for iorb3 in xrange(n_orb):
-                for iorb4 in xrange(n_orb):
+    for iorb1 in np.arange(n_orb):
+        for iorb2 in np.arange(n_orb):
+            for iorb3 in np.arange(n_orb):
+                for iorb4 in np.arange(n_orb):
                     coeff = 0.0
                     if iorb1==iorb2 and iorb2==iorb3 and iorb3==iorb4:
                         coeff = U / 2
-                        for isp in xrange(2):
+                        for isp in np.arange(2):
                             isp2 = 1 - isp
                             U_tensor[iorb1, isp,    iorb2, isp2,
                                      iorb3, isp2,  iorb4, isp] = coeff
@@ -57,18 +56,18 @@ def generate_U_tensor_kunes(n_orb, U, JH, Jprime):
                     elif iorb1==iorb4 and iorb2==iorb3 and iorb1!=iorb2:
                         # this is U', i.e. dd interaction between different orbs
                         coeff = (U - 2 * JH) / 2.0  # divide by 2 for ab neq ba
-                        for isp in xrange(2):
-                            for isp2 in xrange(2):
+                        for isp in np.arange(2):
+                            for isp2 in np.arange(2):
                                 U_tensor[iorb1, isp,    iorb2, isp2,
                                         iorb3, isp2,  iorb4, isp] = coeff
                                 if np.abs(coeff)>0:
                                     num_elem += 1                                    
-                        for isp in xrange(2):
+                        for isp in np.arange(2):
                             U_tensor[iorb1, isp,    iorb2, isp,
                                         iorb3, isp,  iorb4, isp] -= JH / 2.0
                     elif iorb1==iorb3 and iorb2==iorb4 and iorb1!=iorb2:
                         coeff = JH / 2.0  # acount for exchange b <-> a
-                        for isp in xrange(2):
+                        for isp in np.arange(2):
                             isp2 = 1 - isp
                             U_tensor[iorb1, isp, iorb2, isp2,
                                      iorb3, isp2,  iorb4, isp] = coeff
@@ -76,7 +75,7 @@ def generate_U_tensor_kunes(n_orb, U, JH, Jprime):
                                 num_elem += 1
                     elif iorb1==iorb2 and iorb3==iorb4 and iorb1!=iorb3:
                         coeff = Jprime / 2.0
-                        for isp in xrange(2):
+                        for isp in np.arange(2):
                             isp2 = 1 - isp
                             U_tensor[iorb1, isp, iorb2, isp2,
                                      iorb3, isp2,  iorb4, isp] = coeff
@@ -92,86 +91,116 @@ def generate_dd_tensor(n_orb, U, JH):
     U_tensor = np.zeros((n_orb,2,n_orb,2,n_orb,2,n_orb,2),dtype=complex)
 
     num_elem = 0
-    for iorb1 in xrange(n_orb):
-        iorb2 = 1 - iorb1
-        for isp1 in xrange(n_spins):
-            isp2 = 1 - isp1
-            U_tensor[iorb1, isp1, iorb2, isp2,
-                     iorb1, isp1, iorb2, isp2] = -0.5 * U_prime
-            U_tensor[iorb1, isp1, iorb2, isp2,
-                     iorb2, isp2, iorb1, isp1 ] = 0.5 * U_prime
-            num_elem += 2
+    for iorb1 in np.arange(n_orb):
+        for iorb2 in np.arange(n_orb):
+            if (iorb2 != iorb1):
+                for isp1 in np.arange(n_spins):
+                    isp2 = 1 - isp1
+                    if(np.abs(U_tensor[iorb1, isp1, iorb2, isp2,
+                                    iorb1, isp1, iorb2, isp2]) < eps):
+                        num_elem += 1
+                    U_tensor[iorb1, isp1, iorb2, isp2,
+                        iorb1, isp1, iorb2, isp2] += -0.5 * U_prime
+                    if(np.abs(U_tensor[iorb1, isp1, iorb2, isp2,
+                            iorb2, isp2, iorb1, isp1]) < eps):
+                        num_elem += 1
+                    U_tensor[iorb1, isp1, iorb2, isp2,
+                        iorb2, isp2, iorb1, isp1] += 0.5 * U_prime
 
-    for iorb1 in xrange(n_orb):
-        iorb2 = 1 - iorb1
-        for isp1 in xrange(n_spins):
-            isp2 = isp1
-            U_tensor[iorb1, isp1, iorb2, isp2,
-                     iorb1, isp1, iorb2, isp2] = -0.5 * U_prime + 0.5 * JH
-            U_tensor[iorb1, isp1, iorb2, isp2,
-                     iorb2, isp2, iorb1, isp1 ] = 0.5 * U_prime - 0.5 * JH
-            num_elem += 2
+    for iorb1 in np.arange(n_orb):
+        for iorb2 in np.arange(n_orb):
+            if (iorb2 != iorb1):
+                for isp1 in np.arange(n_spins):
+                    isp2 = isp1
+                    if(np.abs(U_tensor[iorb1, isp1, iorb2, isp2,
+                             iorb1, isp1, iorb2, isp2]) < eps):
+                        num_elem += 1
+                    U_tensor[iorb1, isp1, iorb2, isp2,
+                             iorb1, isp1, iorb2, isp2] += -0.5 * U_prime + 0.5 * JH
+                    if(np.abs(U_tensor[iorb1, isp1, iorb2, isp2,
+                             iorb2, isp2, iorb1, isp1]) < eps):
+                        num_elem += 1
+                    U_tensor[iorb1, isp1, iorb2, isp2,
+                             iorb2, isp2, iorb1, isp1] += 0.5 * U_prime - 0.5 * JH
 
-    for iorb1 in xrange(n_orb):
-        iorb2 = iorb1
-        for isp1 in xrange(n_spins):
-            isp2 = 1 - isp1
-            U_tensor[iorb1, isp1, iorb2, isp2,
-                     iorb1, isp1, iorb2, isp2] = -0.5 * U
-            U_tensor[iorb1, isp1, iorb2, isp2,
-                     iorb2, isp2, iorb1, isp1 ] = 0.5 * U
-            num_elem += 2
-    print "found ", num_elem, "non zero coefficients in local Hamiltonian (DD)"
+    for iorb1 in np.arange(n_orb):
+        for iorb2 in np.arange(n_orb):
+            if (iorb2 != iorb1):
+                for isp1 in np.arange(n_spins):
+                    isp2 = 1 - isp1
+                    if(np.abs(U_tensor[iorb1, isp1, iorb2, isp2,
+                                iorb1, isp1, iorb2, isp2]) < eps):
+                        num_elem += 1
+                    U_tensor[iorb1, isp1, iorb2, isp2,
+                             iorb1, isp1, iorb2, isp2] += -0.5 * U
+                    if(np.abs(U_tensor[iorb1, isp1, iorb2, isp2,
+                                iorb2, isp2, iorb1, isp1]) < eps):
+                        num_elem += 1
+                    U_tensor[iorb1, isp1, iorb2, isp2,
+                             iorb2, isp2, iorb1, isp1] += 0.5 * U
+    print ("found ", num_elem, "non zero coefficients in local Hamiltonian (DD)")
     return U_tensor, num_elem
 
 def add_sk_tensor(n_orb, U_tensor, num_elem, JH):
     # adds the SU(2) component to the dd Hamiltonian
     n_spins = 2
-    for iorb1 in xrange(n_orb):
-        iorb2 = 1 - iorb1
-        for isp1 in xrange(n_spins):
-            isp2 = 1 - isp1
-            if (np.abs(U_tensor[iorb1, isp1, iorb2, isp2, iorb1, isp2, iorb2, isp1]) < 0.0000001):
-                num_elem += 2
-            else:
-                print U_tensor[iorb1, isp1, iorb2, isp2, iorb1, isp2, iorb2, isp1]
-            U_tensor[iorb1, isp1, iorb2, isp2,
-                     iorb1, isp2, iorb2, isp1] += 0.25 * JH
-            U_tensor[iorb1, isp1, iorb2, isp2,
-                     iorb2, isp1, iorb1, isp2] -= 0.25 * JH
-    print "found ", num_elem, "non zero coefficients in local Hamiltonian (SK)"
+    for iorb1 in np.arange(n_orb):
+        for iorb2 in np.arange(n_orb):
+            if (iorb2 != iorb1):
+                for isp1 in np.arange(n_spins):
+                    isp2 = 1 - isp1
+                    if (np.abs(U_tensor[iorb1, isp1, iorb2, isp2, iorb1, isp2, iorb2, isp1]) < 0.0000001):
+                        num_elem += 2
+                    else:
+                        print (U_tensor[iorb1, isp1, iorb2, isp2, iorb1, isp2, iorb2, isp1])
+                    U_tensor[iorb1, isp1, iorb2, isp2,
+                             iorb1, isp2, iorb2, isp1] += 0.25 * JH
+                    U_tensor[iorb1, isp1, iorb2, isp2,
+                             iorb2, isp1, iorb1, isp2] -= 0.25 * JH
+    print ("found ", num_elem, "non zero coefficients in local Hamiltonian (SK)")
     return U_tensor, num_elem
 
 n_sites = 2
+n_sites = 3
 Uval = 4.0
+Uval = 3.1
 #Jval = 0.25 * Uval
 Jval = 1.0
+Jval = 0.7
 #Jprime = 0.002
 
 V_mat = np.identity(2 * n_sites, dtype=complex)
 
 U_tensor, num_elem = generate_dd_tensor(n_sites, Uval, Jval)
-U_tensor, num_elem = add_sk_tensor(n_sites, U_tensor, num_elem, 1.0 * Jval)
+#U_tensor, num_elem = add_sk_tensor(n_sites, U_tensor, num_elem, 1.0 * Jval)
 
 corresp = {}
-corresp[(0, 0)] = 0
-corresp[(0, 1)] = 2
-corresp[(1, 0)] = 3
-corresp[(1, 1)] = 1
+if (n_sites == 2):
+    corresp[(0, 0)] = 0
+    corresp[(0, 1)] = 2
+    corresp[(1, 0)] = 3
+    corresp[(1, 1)] = 1
+elif (n_sites == 3):
+    corresp[(0, 0)] = 0
+    corresp[(0, 1)] = 1
+    corresp[(1, 0)] = 2
+    corresp[(1, 1)] = 3
+    corresp[(2, 0)] = 4
+    corresp[(2, 1)] = 5
 
 f = open("Uijkl_SK.txt", "w")
-print >>f, num_elem
+print (num_elem, file=f)
 line = 0
-for iorb1 in xrange(n_sites):
-    for iorb2 in xrange(n_sites):
-        for iorb3 in xrange(n_sites):
-            for iorb4 in xrange(n_sites):
-                for isp1 in xrange(2):
-                    for isp2 in xrange(2):
-                        for isp3 in xrange(2):
-                            for isp4 in xrange(2):
+for iorb1 in np.arange(n_sites):
+    for iorb2 in np.arange(n_sites):
+        for iorb3 in np.arange(n_sites):
+            for iorb4 in np.arange(n_sites):
+                for isp1 in np.arange(2):
+                    for isp2 in np.arange(2):
+                        for isp3 in np.arange(2):
+                            for isp4 in np.arange(2):
                                 if U_tensor[iorb1,isp1,iorb2,isp2,iorb3,isp3,iorb4,isp4] != 0.0:
-                                    print >>f, line, "   ", corresp[(iorb1,isp1)], corresp[(iorb2,isp2)], corresp[(iorb3,isp3)], corresp[(iorb4,isp4)], U_tensor[iorb1,isp1,iorb2,isp2,iorb3,isp3,iorb4,isp4].real, U_tensor[iorb1,isp1,iorb2,isp2,iorb3,isp3,iorb4,isp4].imag
+                                    print (line, "   ", corresp[(iorb1,isp1)], corresp[(iorb2,isp2)], corresp[(iorb3,isp3)], corresp[(iorb4,isp4)], U_tensor[iorb1,isp1,iorb2,isp2,iorb3,isp3,iorb4,isp4].real, U_tensor[iorb1,isp1,iorb2,isp2,iorb3,isp3,iorb4,isp4].imag, file=f)
                                     line += 1
 
 f.close()
--- tutorials/tuto_gfy/gen_hyb.py
+++ tutorials/tuto_gfy/gen_hyb.py
@@ -20,30 +20,30 @@ from pathlib import Path, PurePath
 
 target_location = PurePath(os.environ['HOME'], 'Code', 'Linear_response', 'Source', 'Misc')
 
-print Path.cwd()
 
 if str(target_location) not in sys.path:
     sys.path.append(str(target_location))
-print sys.path
 
 import analyze_output as an
 
 data_root = PurePath(os.environ['HOME'], 'Code', 'Linear_response', 'Data',
-                     'Alps2', 'lr0.4118', '1site', 'Delta_3.6',
-                     'ch0', 'dope1.6', 'b22')
+                         'Alps2', 'lr0.4118', '1site', 'Delta_3.6',
+                        'ch0', 'dope1.6', 'b22')
+
+data_root = os.getcwd()
 
 all_orbitals = ['a_up','b_down','a_down','b_up']
-nb_sites = 1
-nb_orbital_couples = len(all_orbitals)**2
-nb_orbitals = len(all_orbitals * nb_sites)
-n_orbitals = len(all_orbitals)
+all_orbitals = ['a_up','a_down','b_up','b_down', 'c_up','c_down']
 
+n_space_sites = 1
+n_space_sites = 2
+n_sites = 2
+nb_orbital_couples = len(all_orbitals)**2
+nb_orbitals = len(all_orbitals * n_sites)
+n_orbitals = len(all_orbitals)
 
 configs = ['real']
 
-print "data root:\n", data_root
-print
-print "configs: \n", configs
 
 
 
@@ -53,38 +53,37 @@ config = configs[config_index]
 current_path = PurePath(data_root)
 
 gtau2 = {}
-print str(Path(current_path, 'c_delta_0.h5'))
+#print str(Path(current_path, 'c_delta_0.h5'))
 
-print "trying to open", current_path
+#print "trying to open", current_path
 
 corresp = {}
-corresp[0] = 0
-corresp[1] = 1
-corresp[2] = 2
-corresp[3] = 3
+for i in np.arange(n_orbitals):
+    corresp[i] = i
 
 
-with h5.File(str(Path(current_path, 'c_delta_0.h5')), 'r') as input_file:
-    for i in range(n_orbitals * n_orbitals):
-        row_index = i / n_orbitals
-        col_index = i % n_orbitals
-        target_h5_name = '/'
-        my_group = input_file.get(target_h5_name)
-        temp_data = (np.array(my_group.get('Delta_' + str(i))[:, 0] +
-                        my_group.get('Delta_' + str(i))[:, 1] * 1j))
-        # Minus sign: the od_hyb framework has this minus
-        # inherited from Fortran
-        gtau2[i] = -temp_data
 
-output_file = str(Path(Path.cwd(), 'ec_delta.txt'))
+#with h5.File(str(Path(current_path, 'c_delta_0.h5')), 'r') as input_file:
+#    for i in range(n_orbitals * n_orbitals):
+#        row_index = i / n_orbitals
+#        col_index = i % n_orbitals
+#        target_h5_name = '/'
+#        my_group = input_file.get(target_h5_name)
+#        temp_data = (np.array(my_group.get('Delta_' + str(i))[:, 0] +
+#                        my_group.get('Delta_' + str(i))[:, 1] * 1j))
+#        # Minus sign: the od_hyb framework has this minus
+#        # inherited from Fortran
+#        gtau2[i] = -temp_data
+
+output_file = str(Path(Path.cwd(), 'ec_delta.tau'))
 with open(output_file, 'w') as output_file:
-    for tau_index in np.arange(gtau2[i].shape[0]):
+    for tau_index in np.arange(1801):
         for i in np.arange(n_orbitals):
             for j in np.arange(n_orbitals):
                 orb_index = corresp[i] * n_orbitals + corresp[j]
                 out_str = (str(tau_index) + '  ' +
                            str(i) + '  ' +
                            str(j) + '  ' +
-                           str(np.real(gtau2[orb_index][tau_index])) + '  ' +
-                           str(np.imag(gtau2[orb_index][tau_index])) + '\n')
+                           str(0.0) + '  ' +
+                           str(0.0) + '\n')
                 output_file.write(out_str)
